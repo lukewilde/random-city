39971a44ac2fab62dbcced51628935aaa1f663a7
Luke Wilde <luke.wilde@clock.co.uk>
2014-02-22 20:04:30 +0000

    Tidying away js and css.

--- xygen.js
+++ xygen.js
@@ -1,32 +1,3 @@
-// var levelSeed = 51748
-var levelSeeds = [51748, 76294, 52836, 97264, 81537]
-  , dimensions =
-    {  width: 600
-    ,  height: 400
-    }
-
-function generateLevel(levelSeed) {
-
-  var totalPoints = Math.floor(levelSeed / 4000)
-    , points = []
-    , chaosPoints = getChaos(levelSeed, totalPoints * 2)
-
-  console.log('Generating ' + totalPoints + ' coordinates')
-  console.log('Generating chaos: ', chaosPoints)
-
-  for (var i = 0; i < totalPoints; i++) {
-
-      var point =
-        { x: Math.floor(chaosPoints[i] * dimensions.width)
-        , y: Math.floor(chaosPoints[i + 10] * dimensions.height)
-        }
-
-    points[i] = point
-  }
-
-  return points
-}
-
 function logisticMap(x) {
   return 4 * x * (1 - x)
 }
@@ -44,13 +15,3 @@ function getChaos(seed, total) {
 
   return data.splice(100, total)
 }
-
-var levels = []
-
-levelSeeds.forEach(function(seed) {
-  levels.push(generateLevel(seed))
-})
-
-console.log('payload', levels)
-
-// console.log('payload', generateLevel(levelSeed))
